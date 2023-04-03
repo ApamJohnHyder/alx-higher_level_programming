e71119e0e793af53d0f4255e4cc50fd6524dc873
Apam John Hyder <117967310+ApamJohnHyder@users.noreply.github.com>
2023-04-03 16:26:33 +0000

    Compare rectangles

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,1 +1,18 @@
-wqwq
+#!/usr/bin/python3
+"""
+Defines a class Rectangle
+"""
+
+
+class Rectangle:
+    """Representation of a rectangle"""
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
