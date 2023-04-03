3d7518e7aa30121e99ddb8c611aa09ca00cd569d
Apam John Hyder <117967310+ApamJohnHyder@users.noreply.github.com>
2023-04-03 20:09:24 +0000

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 """
-Defines a class Rectangle
+Defines an empty class Rectangle
 """
+
+
 class Rectangle:
-    """Representation of a rectangle"""
-
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def area(self):
-        return self.width * self.height
-    def perimeter(self):
-        return 2 * (self.width + self.height)
+    """Empty representation of a rectangle"""
+    pass
